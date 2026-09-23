dc30acad56e6adb87e2af552389a7c786e34dc02
teemo0017 <teemo0017@hotmail.com>
2026-09-23 06:55:10 -0600

    add test

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <h1>¡Hola! Mi pipeline de CI/CD funciona 🚀</h1>
+    <h1>¡Hola! Mi pipeline de CI/CD funciona Desde Staging 🚀</h1>
   </div>
 </template>
